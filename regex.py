6bd1655a3109daca44e4d20f54bea46cef678c94
MurmuDev <murmudev24@gmail.com>
2020-05-30 15:13:25 +0530

    added documentation for regex.py

--- regex.py
+++ regex.py
@@ -1,5 +1,8 @@
+# This class store regular expressions of python_grammar
+
 import re
 
+#add regular expressions here with code
 RegDict =  {r'(\S+)+\s*=\s*(\S+)+(?!\(\))':0,                   #x = y
             r'from?\s+(S+)+\s+import\s+(\S*)':1,       #import
             r'^(def)\s*(\S+)\((.*?)\)':2,                     #function/method
@@ -11,6 +14,8 @@ RegDict =  {r'(\S+)+\s*=\s*(\S+)+(?!\(\))':0,                   #x = y
             r'(?!=)(\w+)+\((\S+)*\)':8 ,                          #caller()
             }
 
+
+# returns groups of regex if found
 def compareReg(reg,line):
     x = re.compile(reg)
     regobj = x.search(line)
@@ -20,7 +25,7 @@ def compareReg(reg,line):
         return regobj.groups()
 
 
-
+# returns a tuple of groups appended with code at last
 def regexCheck(line):
     global RegDict
     for x,y in RegDict.items():
